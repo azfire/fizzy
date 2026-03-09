23346bd74604c3d4bfc594c2c981119b006376f3
Filipe Guedes <filipeguedes@hey.com>
2026-03-09 19:05:22 +0000

    Add infinite option to auto close

--- app/helpers/entropy_helper.rb
+++ app/helpers/entropy_helper.rb
@@ -1,6 +1,12 @@
 module EntropyHelper
+  INFINITE_AUTO_CLOSE_DAYS = 36500
+
   def entropy_auto_close_options
-    [ 3, 7, 30, 90, 365, 11 ]
+    [ 3, 7, 30, 90, 365, 11, INFINITE_AUTO_CLOSE_DAYS ]
+  end
+
+  def entropy_label_for(days)
+    days == INFINITE_AUTO_CLOSE_DAYS ? "∞" : days
   end
 
   def entropy_bubble_options_for(card)
